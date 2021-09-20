215892c24300d32738f0004f4ceb03052dba1b01
remy-duez <remy.duez@laplateforme.io>
2021-09-20 13:09:55 +0200

    adds controllers for Posts, Category and Main pages

---
+++ src/Controller/PostsController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PostsController extends AbstractController
+{
+    /**
+     * @Route("/posts", name="posts")
+     */
+    public function index(): Response
+    {
+        return $this->render('posts/index.html.twig', [
+            'controller_name' => 'PostsController',
+        ]);
+    }
+}
