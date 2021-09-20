f0cf6bc6170204e1b35130d08117052536060fc3
remy-duez <remy.duez@laplateforme.io>
2021-09-20 13:17:54 +0200

    adds relations migrations

---
+++ migrations/Version20210920111651.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210920111651 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE post ADD category_id INT DEFAULT NULL, ADD author_id INT NOT NULL');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8D12469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('ALTER TABLE post ADD CONSTRAINT FK_5A8A6C8DF675F31B FOREIGN KEY (author_id) REFERENCES user (id)');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8D12469DE2 ON post (category_id)');
+        $this->addSql('CREATE INDEX IDX_5A8A6C8DF675F31B ON post (author_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8D12469DE2');
+        $this->addSql('ALTER TABLE post DROP FOREIGN KEY FK_5A8A6C8DF675F31B');
+        $this->addSql('DROP INDEX IDX_5A8A6C8D12469DE2 ON post');
+        $this->addSql('DROP INDEX IDX_5A8A6C8DF675F31B ON post');
+        $this->addSql('ALTER TABLE post DROP category_id, DROP author_id');
+    }
+}
