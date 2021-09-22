b94a4c12a7b17b0c2c87d60de4e54b4f3c7e5d9c
remy-duez <remy.duez@laplateforme.io>
2021-09-22 12:44:17 +0200

    Adds pagination, show_post and posts pages and its logic in controller

--- src/Controller/PostsController.php
+++ src/Controller/PostsController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +15,20 @@ class PostsController extends AbstractController
     /**
      * @Route("/posts", name="posts")
      */
-    public function index(): Response
+    public function index(Request $request, PaginatorInterface $paginator, PostRepository $postRepository): Response
     {
-        return $this->render('posts/index.html.twig', [
-            'controller_name' => 'PostsController',
+        $data = $postRepository->findBy([],['createdAt' => 'desc']);
+        $posts = $paginator->paginate($data, $request->query->getInt('page', 1), 12);
+        return $this->render('posts/posts.html.twig', [
+            'posts' => $posts,
         ]);
+    }
+
+    /**
+     * @Route("/posts/{id<\d+>}", name="show_post")
+     */
+    public function showArticle(Post $post) : Response
+    {
+        return $this->render('posts/show_post.html.twig', compact('post'));
     }
 }
