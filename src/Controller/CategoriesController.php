5bc4d0e6ce809c238fed07b23ab149f335b49556
remy-duez <remy.duez@laplateforme.io>
2021-09-23 08:41:47 +0200

    adds new show category route and logic

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Category;
 use App\Repository\CategoryRepository;
 use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,6 +20,20 @@ class CategoriesController extends AbstractController
     {
         return $this->render('categories/categories.html.twig', [
             'posts' => $postRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/categories/{id<\d+>}", name="show_category")
+     */
+    public function showCategory(CategoryRepository $categoryRepository,PostRepository $postRepository,PaginatorInterface $paginator,Request $request, $id): Response
+    {
+        $category = $categoryRepository->findOneBy(['id' => $id]);
+        $data = $postRepository->findBy(['category' => $category],['createdAt'=> 'desc']);
+        $posts = $paginator->paginate($data, $request->query->getInt('page', 1), 8);
+        return $this->render('categories/category.html.twig', [
+            'id' => $id,
+            'posts' => $posts
         ]);
     }
 }
