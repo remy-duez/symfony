fd34987c10ced071faca011cf02074436a9b092e
remy-duez <remy.duez@laplateforme.io>
2021-09-23 08:40:14 +0200

    Modifies email field

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,7 +16,7 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email',null, [
+            ->add('email',EmailType::class, [
                 'label' => false,
                 'attr' => [
                     'placeholder' => 'Your email',
